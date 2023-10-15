c4d865276b0ba0244fef8d18ad01fb5470e5b603
kyleyang2020 <kyleyang2020@gmail.com>
2023-10-14 23:33:50 -0400

    started health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    // variables for starting and current health
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+
+    // assigning variables, setting current health
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        // remove health equal to damage
+        currentHealth -= _damage;
+
+        // is player is hurt
+        if (currentHealth > 0)
+        {
+            
+        }
+        // if player is dead
+        else
+        {
+            
+        }
+    }
+}
